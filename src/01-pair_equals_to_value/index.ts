4666afac0a6a1ecb8ae564ac94af7d5de592e8f5
fabien <fabien.andrian@gmail.com>
2022-10-06 00:19:07 +0200

    update readmes for 01 and 02

--- src/01-pair_equals_to_value/index.ts
+++ src/01-pair_equals_to_value/index.ts
@@ -5,22 +5,6 @@ import {
     getPairWithSmallerIndexes
 } from './main'
 
-/*
-Given an integer "k" and array "arr"
-Use the array "arr" to find the pairs (ie. couples) for which the sum equals to k.
-If multiples responses are possible, you should :
-- start from left to right (ex: indexes 1-4 is better than 2-5; and 2-4 is better 2-5)
-- not take account of "mirror indexes" (ie. if left + right = sum, then ignore right + left)
-- not take account twice of the same index twice (ie. if left + left = sum, then ignore)
-- return an empty array if no pairs equals to sum
-
-Example :
-- k = 2
-- arr = [5, 2, 1, 3, 6, 4]
-- Possibles responses : (5 + 2) ; (3 + 4) ; (4 + 3)
-- Response: (5 + 2) --> as indexes : (0, 1)
-
-*/
 
 const k = 7
 const arr = [5, 3, 4, 2, 1, 3, 6, 4, 2, 5]
---
+++ src/02-coin_change/readme.md
@@ -0,0 +1,25 @@
+Find the optimized combination of currency to equal an amount of money.
+Optimized means you have to use less coins / billets as possible.
+Only 2€, 5€ and 10€ (coins / billets) are allowed.
+
+Example : 
+- Total = 45€
+- Possible responses : [ 4*ten + 1*five ; 9*five ; 20*two + 1*five ; 2*ten + 1*five + 10*two, ... ]
+- Optimized response = 4*ten + 1*five
+
+Impossible: 1€ et 3€
+
+1 = ***
+2 = 1*two
+3 = ***
+4 = 2*two
+5 = 1*five
+6 = 3*two
+7 = 1*five + 1*deux
+8 = 4*two
+9 = 1*five + 2*two
+10 = 1*ten  
+
+Warning : 
+- 11 = 5 + 6 = 1*five + 3*two
+- 13 = 5 + 8 = 1*five + 4*two
