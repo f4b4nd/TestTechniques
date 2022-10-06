756121fde3d70734c800f82afc99c5a87a90fdf9
fabien <fabien.andrian@gmail.com>
2022-10-06 18:53:27 +0200

    add convert to base 10 exo

--- src/05-number_base_conversion/index.test.ts
+++ src/05-number_base_conversion/index.test.ts
@@ -1,14 +1,30 @@
 import { describe, expect, it } from '@jest/globals'
 
-import { getNumberBaseConversion } from '.'
+import { getConvertedFromBase10, getConvertedToBase10 } from '.'
 
-describe('Given an integer n and base b', () => {
+describe('Given an integer n in base 10, and a target base b', () => {
     it('should return n converted in base b', () => {
 
-        const n1 = 1830, b1 = 2, res1 = 11100100110
-        expect(getNumberBaseConversion(n1, b1)).toEqual(res1)
+        const n1 = 1830, b1 = 2, res1 = '11100100110'
+        expect(getConvertedFromBase10(n1, b1)).toBe(res1)
 
-        const n2 = 1830, b2 = 16, res2 = 726
-        expect(getNumberBaseConversion(n2, b2)).toEqual(res2)
+        const n2 = 1830, b2 = 16, res2 = '726'
+        expect(getConvertedFromBase10(n2, b2)).toBe(res2)
+
+        const n3 = 209963, b3 = 62, res3 = 'scV'
+        expect(getConvertedFromBase10(n3, b3)).toBe(res3)
+    })
+})
+
+
+describe('Given an integer n in base b', () => {
+    it('should return n converted in base 10', () => {
+
+        const n1 = '1FA6', b1 = 16, res1 = 8102
+        expect(getConvertedToBase10(n1, b1)).toBe(res1)
+
+        const n2 = '10011', b2 = 2, res2 = 19
+        expect(getConvertedToBase10(n2, b2)).toBe(res2)
+
     })
 })
--- src/05-number_base_conversion/index.ts
+++ src/05-number_base_conversion/index.ts
@@ -1,24 +1,49 @@
 const getDivision = (a: number, b: number) => {
     return {
-        quotient: Math.floor(a / b),
+        truncatedQuotient: Math.floor(a / b),
         remainder: a % b,
     }
 }
 
-const getSuccessiveRemainders = (a: number, b: number): number[] => {
- 
-    const {quotient, remainder} = getDivision(a, b)
+const digitToCharacter = (digit: number): string => {
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    if (digit < chars.length) return chars[digit]
+    throw 'Cannot represent digit in base > 62'
+}
 
+const characterToDigit = (char: string): number => {
+    if (char.length !== 1) throw 'Character length must equal to 1'
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split("")
+    const matchIndex = chars.reduce((acc, curr, idx) => (!acc && curr === char) ? idx : acc, NaN)
+    if (matchIndex || matchIndex === 0) return matchIndex
+    throw `No character found for char: ${char}`
+}
+
+const getSuccessiveRemainders = (a: number, b: number): string[] => {
+ 
     if (a === 0) return []
 
-    return [remainder, ...getSuccessiveRemainders(quotient, b)]
+    const {truncatedQuotient, remainder} = getDivision(a, b)
+
+    const remainderAsChar = digitToCharacter(remainder)
+
+    return [remainderAsChar, ...getSuccessiveRemainders(truncatedQuotient, b)]
 
 }
 
-export const getNumberBaseConversion = (n: number, base: number): number => {
+export const getConvertedFromBase10 = (n: number, base: number): string => {
     const remainders = getSuccessiveRemainders(n, base)
     const numberConversion = remainders.reverse().join("")
-    return parseInt(numberConversion)
+    return numberConversion
 }
 
-//console.log(getNumberBaseConversion(1830, 2))
+export const getConvertedToBase10 = (n: string, base: number): number => {
+    const digits = n.split("").map(c => characterToDigit(c))
+    const result = digits.map((digit, idx, t) => digit * (base ** (t.length - 1 - idx)))
+    const sum = result.reduce((a, b) => a + b)
+    return sum
+}
+
+console.log(getConvertedFromBase10(209963, 62))
+console.log(getConvertedToBase10('1FA6', 16))
+console.log(getConvertedToBase10('10011', 2))
